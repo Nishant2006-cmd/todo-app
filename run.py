c56d581b154eff94967d318c11d57f427bc5c980
Nishant Sharma <nishant.1511977@gmail.com>
2026-03-20 14:54:45 +0530

    i changed like added admin and 1234 id pass becuse i havent able to add registration page

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+import os
 from app import create_app, db
-from app.models import Task
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+
 print("RUN.PY EXECUTED")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
